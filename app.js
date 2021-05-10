cc6ae33939c7838ca20d1a75b3e4b599ec6b4ba5
Evgeniy <ghenya.t@gmail.com>
2021-05-10 22:29:57 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,106 +1,100 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
-
-mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const fruitSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: 1
-    },
-    rating: Number
-
-})
+  name: {
+    type: String,
+    required: 1,
+  },
+  rating: Number,
+});
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const peopleSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Why no name']},
-    age: Number,
-    favoritFruit: fruitSchema
+  name: {
+    type: String,
+    required: [true, "Why no name"],
+  },
+  age: Number,
+  favoritFruit: fruitSchema,
 });
 
 const Human = mongoose.model("Human", peopleSchema);
 
 const apple = new Fruit({
-    name: 'apple',
-    rating: 7
-})
+  name: "apple",
+  rating: 7,
+});
 
 const human = new Human({
-    name: 'John',
-    age: 37,
-    favoritFruit: apple
-}); 
-const woman = new Human({
-    name: 'Jane',
-    age: 28
+  name: "John",
+  age: 37,
+  favoritFruit: apple,
 });
- 
+const woman = new Human({
+  name: "Jane",
+  age: 28,
+});
+
 (async () => {
+  await apple.save();
+  await human.save();
+  console.log("New items saved");
 
-    await apple.save();
-    await human.save();
-    console.log("ok");
-    const repFruits = await Fruit.aggregate([
-        {
-         $group: {
-            _id: { name: "$name" },
-            dups: { $addToSet: "$_id"  },
-            count: { $sum: 1 }
-         }},         
-      { 
-   $match:
-     {
-       count: {"$gt": 1}
-     }
- }
-        
-        ]).exec();
-        repFruits.forEach( (doc) => {
-         console.log( doc.dups.shift());  
+  const repFruits = await Fruit.aggregate([
+    {
+      $group: {
+        _id: { name: "$name" },
+        dups: { $addToSet: "$_id" },
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $match: {
+        count: { $gt: 1 },
+      },
+    },
+  ]).exec();
 
-          Fruit.deleteMany({
-              _id: {$in: doc.dups}
-               }, (err) => {
-                console.log(err);
-                              });
-                    
-        });
+  const repPeople = await Human.aggregate([
+    {
+      $group: {
+        _id: { name: "$name" },
+        dups: { $addToSet: "$_id" },
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $match: {
+        count: { $gt: 1 },
+      },
+    },
+  ]).exec();
+  let idsToDelete = [];
+  idsToDelete = repPeople.reduce(
+    (arr, current) => [...arr, ...current.dups.slice(1)],
+    []
+  );
 
-        const repPeople = await Human.aggregate([
-            {
-             $group: {
-                _id: { name: "$name" },
-                dups: { $addToSet: "$_id"  },
-                count: { $sum: 1 }
-             }},         
-          { 
-       $match:
-         {
-           count: {"$gt": 1}
-         }
-     }
-            
-            ]).exec();
-            repPeople.forEach( (doc) => {
-             console.log( doc.dups.shift());  
+  if (idsToDelete.length) {
+    await Human.deleteMany({ _id: { $in: idsToDelete } });
+  }
 
-    
-              Human.deleteMany({
-                  _id: {$in: doc.dups}
-                   }, (err) => {
-                    console.log(err);
-                                  });
-                        
-            });
-       
-            const fruits = await Fruit.find({});
-            const people = await Human.find({});
-            console.log("people " + people);
-            console.log("Fruits " + fruits);
-            mongoose.disconnect();             
+  idsToDelete = repFruits.reduce(
+    (arr, current) => [...arr, ...current.dups.slice(1)],
+    idsToDelete
+  );
+  console.log("idsToDelete  " + idsToDelete);
+  if (idsToDelete.length) {
+    await Fruit.deleteMany({ _id: { $in: idsToDelete } });
+  }
+
+  const [fruits, people] = await Promise.all([Fruit.find({}), Human.find({})]);
+  console.log("people " + people);
+  console.log("Fruits " + fruits);
+  mongoose.disconnect();
 })().catch(console.error);
-
-
